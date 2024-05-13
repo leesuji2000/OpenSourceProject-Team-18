05385a4db9f843bdb9e09241808cbbfdaa8868f2
soyeon1806 <ksy1118s@naver.com>
2024-05-13 21:50:42 +0900

    Files for testing bad word prediction models
    욕설 예측 모델 테스트

---
+++ test.py
@@ -0,0 +1,10 @@
+from badword_check import BadWord
+
+model = BadWord.load_badword_model()
+data = BadWord.preprocessing("ㅅㅂ")
+print(model.predict(data))
+
+if(model.predict(data) >= 0.65):
+    print("욕설입니다.")
+else:
+    print("욕설이 아닙니다.")
